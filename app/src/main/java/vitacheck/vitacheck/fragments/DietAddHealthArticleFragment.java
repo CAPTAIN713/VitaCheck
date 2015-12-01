3c0c8fb8aa65ff453647c2d3c6aeceefb812292c
btterrette7 <btterrette7@verizon.net>
2015-12-01 15:41:56 -0600

    Adding  all the adding fragments, food fragment has a way to save dates if they are in text format.

---
+++ app/src/main/java/vitacheck/vitacheck/fragments/DietAddHealthArticleFragment.java
@@ -0,0 +1,53 @@
+package vitacheck.vitacheck.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import com.parse.ParseObject;
+
+
+import vitacheck.vitacheck.R;
+
+public class DietAddHealthArticleFragment extends Fragment {
+
+
+    Button saveHealthArticleButton;
+    private EditText mHealthName;
+    private EditText mHealthURL;
+    private EditText mHealthDescription;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =inflater.inflate(R.layout.fragment_diet_health_add, container, false);
+
+
+        saveHealthArticleButton = (Button) view.findViewById(R.id.saveHealthButton);
+        saveHealthArticleButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mHealthName = (EditText) getView().findViewById(R.id.health_name);
+                mHealthURL = (EditText) getView().findViewById(R.id.health_URL);
+                mHealthDescription = (EditText) getView().findViewById(R.id.health_description);
+                ParseObject health = new ParseObject("diet_health_article");
+                health.put("name", mHealthName.getText().toString());
+                health.put("URL", mHealthURL.getText().toString());
+                health.put("note", mHealthDescription.getText().toString());
+
+                health.saveInBackground();
+            }
+        });
+        return view;
+
+    }
+
+    private void saveRecipe()
+    {
+
+    }
+}
