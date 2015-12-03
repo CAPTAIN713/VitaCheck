d6fb0ac160ab437b0e2d7a5aa64dd53f5758df10
ERIC <ericb713@gmail.com>
2015-12-02 21:02:01 -0600

    Doctor individual page can now go to the apps google maps, email, phone app, and web browser by click on items in the doctor view page. Had to change the layouts in the doctor view to include the buttons. comments in code has links on how I did it.
    ALSO CHANGED THE APPS DEFAULT ICON!!!!!!!!!!!!!!!!!!! :D

--- app/src/main/java/vitacheck/vitacheck/fragments/DoctorFragmentIndividualPage.java
+++ app/src/main/java/vitacheck/vitacheck/fragments/DoctorFragmentIndividualPage.java
@@ -4,6 +4,9 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +25,10 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +47,13 @@ public class DoctorFragmentIndividualPage extends Fragment implements View.OnCli
     private TextView doctorNameTB,doctorTypeTB,insuranceTB,phoneNumberTB,emailTB;
     private TextView addressTB,websiteTB,visitDateTB;
     private Button editButton;
+    private ImageButton callButton;
+    private ImageButton emailButton;
+
+    private String phoneNumber;
+    private String email;
+    private String doctorWebsite;
+    private String address;
 
     private String selectedItemParseID;
     private  Bundle extrasBundle;
@@ -73,8 +88,14 @@ public class DoctorFragmentIndividualPage extends Fragment implements View.OnCli
         visitDateTB=(TextView) layout.findViewById(R.id.doctorVistDatesIndv);
 
         editButton = (Button) layout.findViewById(R.id.editDoctorButton);
+        callButton = (ImageButton) layout.findViewById(R.id.doctorPhoneCallButton);
+        emailButton = (ImageButton) layout.findViewById(R.id.doctorEmailButton);
 
         editButton.setOnClickListener(this);
+        callButton.setOnClickListener(this);
+        emailButton.setOnClickListener(this);
+        websiteTB.setOnClickListener(this);
+        addressTB.setOnClickListener(this);
 
         ParseObject.registerSubclass(DoctorInfo.class);
         ParseQuery<DoctorInfo> query = ParseQuery.getQuery("doctor");
@@ -86,9 +107,13 @@ public class DoctorFragmentIndividualPage extends Fragment implements View.OnCli
                     doctorTypeTB.setText(object.getDoctorType());
                     insuranceTB.setText(object.getInsurance());
                     phoneNumberTB.setText(String.valueOf(object.getPhoneNum()));
+                    phoneNumber=String.valueOf(object.getPhoneNum());
                     emailTB.setText(object.getEmail());
+                    email=String.valueOf(object.getEmail());
                     addressTB.setText(object.getAddress());
+                    address=String.valueOf(object.getAddress());
                     websiteTB.setText(object.getURL());
+                    doctorWebsite=String.valueOf(object.getURL());
                     visitDateTB.setText(DateFormat.getDateInstance().format(object.getVisitDate()));
                 }
                 else{
@@ -112,6 +137,55 @@ public class DoctorFragmentIndividualPage extends Fragment implements View.OnCli
                 doctorEditFragment.setArguments(extrasBundle);
                 transaction.replace(R.id.doctorActivityContainer, doctorEditFragment);
                 transaction.commit();
+                break;
+
+            case R.id.doctorPhoneCallButton:
+                if(phoneNumber.isEmpty()){ break; }
+
+                //link on using phone's call app: http://www.tutorialspoint.com/android/android_phone_calls.htm
+                Intent phoneCallIntent=new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+phoneNumber));
+                try{ startActivity(phoneCallIntent); }
+                catch (android.content.ActivityNotFoundException ex){
+                    Toast.makeText(getActivity(),"Unable to call number",Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.doctorEmailButton:
+                //link on sending emails: http://www.tutorialspoint.com/android/android_sending_email.htm
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setData(Uri.parse("mailto:"));
+                emailIntent.setType("text/plain");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
+                try{
+                    startActivity(Intent.createChooser(emailIntent,"Send mail..."));
+                }
+                catch (android.content.ActivityNotFoundException ex){
+                    Toast.makeText(getActivity(),"Unable to send email",Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.doctorWebsiteIndv:
+                //link to video on going to web browser: https://www.youtube.com/watch?v=9-3OCc5g5oE
+                try {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(doctorWebsite));
+                    startActivity(browserIntent);
+                }
+                catch (android.content.ActivityNotFoundException ex){
+                    Toast.makeText(getActivity(),"Unable to open link",Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.doctorAddressIndv:
+                /*video and link on opening maps: https://www.youtube.com/watch?v=iGbMNfv2KxA
+                https://developers.google.com/maps/documentation/android-api/intents?hl=en  */
+                try {
+                    Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+                    mapIntent.setData(Uri.parse("geo:0,0?q="+address));
+                    startActivity(mapIntent);
+                }
+                catch (android.content.ActivityNotFoundException ex){
+                    Toast.makeText(getActivity(),"Unable to open link",Toast.LENGTH_SHORT).show();
+                }
                 break;
         }
     }
