cf4ddd36a63393b487665ed229eca570ea9cd29e
Michael Kosir <mahkosir@gmail.com>
2015-12-04 04:03:19 -0600

    Updates to home page (Cards!), Changed app icon to higher quality

--- app/src/main/java/vitacheck/vitacheck/fragments/HomeFragment.java
+++ app/src/main/java/vitacheck/vitacheck/fragments/HomeFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import vitacheck.vitacheck.MainActivity;
 import vitacheck.vitacheck.R;
 
@@ -13,6 +12,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ((MainActivity) getActivity()).setTitle("Home");
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
