f1222f9dbf4b5e02b8c73fbe3ea15ed5b312d63f
Robert <robert.burton@mavs.uta.edu>
2015-12-06 08:25:20 -0600

    Medication is now fully functional. You can now pick time for medication alarm. Still having problems getting the colors to match our apps theme.

--- app/src/main/java/vitacheck/vitacheck/fragments/MedicineFragmentSaveNewMed.java
+++ app/src/main/java/vitacheck/vitacheck/fragments/MedicineFragmentSaveNewMed.java
@@ -1,13 +1,17 @@
 package vitacheck.vitacheck.fragments;
 
+import android.app.AlarmManager;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,6 +19,9 @@ import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+
+import java.util.Calendar;
+import java.util.Random;
 
 import vitacheck.vitacheck.R;
 
@@ -28,6 +35,8 @@ public class MedicineFragmentSaveNewMed  extends Fragment implements View.OnClic
 
     private EditText medicineNameTB,medicineNoteTB,dosageAmountTB,dosagePerDayTB,prescribedByTB;
     private Button saveButton;
+    private CheckBox reminderCheckBox;
+    AlarmTimePicker medicineTimePicker;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,6 +53,11 @@ public class MedicineFragmentSaveNewMed  extends Fragment implements View.OnClic
 
         saveButton = (Button) layout.findViewById(R.id.medicineSaveNewMedicineButton);
         saveButton.setOnClickListener(this);
+
+        reminderCheckBox = (CheckBox) layout.findViewById(R.id.reminderCheckBox);
+        reminderCheckBox.setOnClickListener(this);
+
+        medicineTimePicker = (AlarmTimePicker) layout.findViewById(R.id.medicineTimePicker);
 
         return layout;
     }
@@ -73,6 +87,28 @@ public class MedicineFragmentSaveNewMed  extends Fragment implements View.OnClic
 
                 newMedicine.saveInBackground();
                 Toast.makeText(context, "Saved", Toast.LENGTH_SHORT).show();
+
+                if(reminderCheckBox.isChecked()) {
+                    Context baseContext = getActivity().getApplicationContext();
+                    Intent intent = new Intent(context, MedicineActivityAlarmPopup.class);
+                    intent.putExtra("name", medicineNameTB.getText().toString());
+                    intent.putExtra("dosage", dosageAmountTB.getText().toString());
+                    int id = new Random().nextInt(100);
+                    PendingIntent pendingIntent = PendingIntent.getActivity(context, id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    Calendar alarmCalendar = Calendar.getInstance();
+                    int timePickerMinute = medicineTimePicker.getCurrentMinute();
+                    int timePickerHour = medicineTimePicker.getCurrentHour();
+                    //If picked time has already passed, "roll" date forward.
+                    if (timePickerHour < alarmCalendar.get(Calendar.HOUR))
+                        alarmCalendar.roll(Calendar.DATE, true);
+                    else if (timePickerHour == alarmCalendar.get(Calendar.HOUR) && timePickerMinute <= alarmCalendar.get(Calendar.MINUTE))
+                        alarmCalendar.roll(Calendar.DATE, true);
+                    alarmCalendar.set(Calendar.HOUR, timePickerHour);
+                    alarmCalendar.set(Calendar.MINUTE, timePickerMinute);
+                    AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+                    alarmManager.set(AlarmManager.RTC_WAKEUP, alarmCalendar.getTimeInMillis(), pendingIntent);
+                    Toast.makeText(context, "Alarm set for " + alarmCalendar.getTime(), Toast.LENGTH_LONG).show();
+                }
 
                 Fragment fragment = new MedicineFragment();
                 FragmentTransaction transaction = getFragmentManager().beginTransaction();
